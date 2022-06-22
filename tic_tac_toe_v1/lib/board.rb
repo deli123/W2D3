bdd01c16ea61e2cb1d431aa954b8e9d60ce3c94b
Derek <liderek63@gmail.com>
2022-06-22 16:50:03 -0700

    completed board.rb

--- tic_tac_toe_v1/lib/board.rb
+++ tic_tac_toe_v1/lib/board.rb
@@ -27,4 +27,50 @@ class Board
 
         self[pos] = mark
     end
+
+    def print
+        @grid.each do |row|
+            puts row.join(" ")
+        end
+    end
+
+    def win_row?(mark)
+        @grid.each do |row|
+            if row.all? { |m| m == mark}
+                return true
+            end
+        end
+        return false
+    end
+
+    def win_col?(mark)
+        transposed = @grid.transpose
+        transposed.each do |row|
+            if row.all? { |m| m == mark}
+                return true
+            end
+        end
+        return false
+    end
+
+    def win_diagonal?(mark)
+        if @grid[0][0] == mark && @grid[1][1] == mark && @grid[2][2] == mark
+            return true
+        elsif @grid[0][2] == mark && @grid[1][1] == mark && @grid[2][0] == mark
+            return true
+        else
+            return false
+        end
+    end
+
+    def win?(mark)
+        return win_row?(mark) || win_col?(mark) || win_diagonal?(mark)
+    end
+
+    def empty_positions?
+        @grid.each do |row|
+            return true if row.any? { |m| m == "_" }
+        end
+        return false
+    end
 end
