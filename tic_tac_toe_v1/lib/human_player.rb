06f0338574817ea745d68e53b6b656df08558ef0
Derek <liderek63@gmail.com>
2022-06-22 16:50:12 -0700

    completed human_player.rb

---
+++ tic_tac_toe_v1/lib/human_player.rb
@@ -0,0 +1,34 @@
+class HumanPlayer
+    attr_reader :mark
+    
+    def initialize(mark)
+        @mark = mark
+    end
+
+    def get_position
+        begin
+            print "Player #{@mark}, enter two numbers representing a position in the format `row col`: "
+            input = gets.chomp.split(" ")
+            valid_input?(input)
+        rescue StandardError => e
+            puts e.message
+            retry
+        end
+        return [input[0].to_i, input[1].to_i]
+    end  
+
+    def valid_input?(input)
+        if input.length != 2
+            raise RuntimeError.new "Sorry, that was invalid"
+        end
+        row, col = input
+        row = row.to_i
+        col = col.to_i
+        if !row.instance_of?(Integer) && !col.instance_of?(Integer)
+            raise RuntimeError.new "Sorry, that was invalid"
+        end
+        if row < 0 || row > 2 || col < 0 || col > 2
+            raise RuntimeError.new "Sorry, that was invalid"
+        end
+    end
+end
