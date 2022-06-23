cab27116ff9f3801a66b8a9c3a0f1c7e776f586f
Derek <liderek63@gmail.com>
2022-06-22 23:30:08 -0700

    completed mastermind.rb

--- mastermind_project/lib/mastermind.rb
+++ mastermind_project/lib/mastermind.rb
@@ -1,5 +1,20 @@
 require_relative "code"
 
 class Mastermind
+    def initialize(length)
+        @secret_code = Code.random(length)
+    end
 
+    def print_matches(code)
+         puts "#{@secret_code.num_exact_matches(code)} exact matches."
+         puts "#{@secret_code.num_near_matches(code)} near matches."
+    end
+
+    def ask_user_for_guess
+        print "Enter a code: "
+        input_str = gets.chomp
+        input = Code.from_string(input_str)
+        self.print_matches(input)
+        @secret_code == input
+    end
 end
